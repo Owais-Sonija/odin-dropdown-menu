9bd22471d9364402a41da852583e3df7dca8e1e3
Owais-Sonija <owaissonija007@gmail.com>
2024-09-12 19:55:51 +0500

    Completed DropDown Functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Heloo");
+// Getting element
+const dropBtn = document.querySelector(".btn");
+const menuEle = document.querySelector(".dropdown-menu");
+
+
+
+
+// Creating function
+function showMenu() {
+    menuEle.classList.remove("-translate-y-4")
+  menuEle.classList.toggle("hidden")
+    
+}
+
+// Calling function
+
+dropBtn.addEventListener("click", showMenu)
